f880b996d87c634edc7791570e427cb1ee12cab3
Andrew Marshall <andrew.marshall@skybettingandgaming.com>
2022-06-08 00:22:41 +0100

    Improved team stats display

--- src/components/teamRecords.js
+++ src/components/teamRecords.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { capitalizeText } from '../helpers/utils';
 
 function TeamRecords(props) {
     const statsArray = props.stats;
 
-    let bestWinPercentage = 0;
-    let bestWinPercentageTeam = '';
-    let bestTeamPointsPerGame = 0;
-    let bestTeamPointsPerGameTeam = '';
-    let bestTeamAggPerGame = 0;
-    let bestTeamAggPerGameTeam = '';
+    let bestWinPercentage = -1;
+    let bestWinPercentageTeam = [];
+    let bestTeamPointsPerGame = -1;
+    let bestTeamPointsPerGameTeam = [];
+    let bestTeamAggPerGame = -1;
+    let bestTeamAggPerGameTeam = [];
     let fewestPointsConcededPerGame = 100;
-    let fewestPointsConcededPerGameTeam = '';
-    let fewestAggConcededPerGame = 1000;
-    let fewestAggConcededPerGameTeam = '';
+    let fewestPointsConcededPerGameTeam = [];
+    let lowestAggConcededPerGame = 1000;
+    let lowestAggConcededPerGameTeam = [];
 
     statsArray.forEach(stats => {
         const {
@@ -32,43 +33,57 @@ function TeamRecords(props) {
         const aggPerGame = (stanningleyAgg / gamesPerMatch) / totalGames;
         const pointsConcededPerGame = (opponentTotalPoints / gamesPerMatch) / totalGames;
         const aggConcededPerGame = (opponentAgg / gamesPerMatch) / totalGames;
-        // TODO handle when values are the same. Sort by games played? Use an array instead?
-        // TODO 2 decimal places?
-        if (aggPerGame > bestTeamAggPerGame) {
+
+        if (aggPerGame >= bestTeamAggPerGame) {
+            if (aggPerGame !== bestTeamAggPerGame) {
+                bestTeamAggPerGameTeam.pop();
+            }
+            bestTeamAggPerGameTeam.push(day);
             bestTeamAggPerGame = aggPerGame;
-            bestTeamAggPerGameTeam = day;
         }
-        if (pointsPerGame > bestTeamPointsPerGame) {
+        if (pointsPerGame >= bestTeamPointsPerGame) {
+            if (pointsPerGame !== bestTeamPointsPerGame) {
+                bestTeamPointsPerGameTeam.pop();
+            }
+            bestTeamPointsPerGameTeam.push(day);
             bestTeamPointsPerGame = pointsPerGame;
-            bestTeamPointsPerGameTeam = day;
         }
-        if (pointsConcededPerGame < fewestPointsConcededPerGame) {
+        if (pointsConcededPerGame <= fewestPointsConcededPerGame) {
+            if (pointsConcededPerGame !== fewestPointsConcededPerGame) {
+                fewestPointsConcededPerGameTeam.pop();
+            }
+            fewestPointsConcededPerGameTeam.push(day);
             fewestPointsConcededPerGame = pointsConcededPerGame;
-            fewestPointsConcededPerGameTeam = day;
         }
-        if (aggConcededPerGame < fewestAggConcededPerGame) {
-            fewestAggConcededPerGame = aggConcededPerGame;
-            fewestAggConcededPerGameTeam = day;
+        if (aggConcededPerGame <= lowestAggConcededPerGame) {
+            if (aggConcededPerGame !== lowestAggConcededPerGame) {
+                lowestAggConcededPerGameTeam.pop();
+            }
+            lowestAggConcededPerGameTeam.push(day);
+            lowestAggConcededPerGame = aggConcededPerGame;
         }
-        if (winPercentage > bestWinPercentage) {
+        if (winPercentage >= bestWinPercentage) {
+            if (winPercentage !== bestWinPercentage) {
+                bestWinPercentageTeam.pop();
+            }
+            bestWinPercentageTeam.push(day);
             bestWinPercentage = winPercentage;
-            bestWinPercentageTeam = day;
         }
     });
 
     return (
         <div id='TeamRecords'>
             <h2>Team Records</h2>
-            <p>Best win percentage = {bestWinPercentage}%</p>
-            <p>Best win percentage Team = {bestWinPercentageTeam}</p>
-            <p>Best points per game = {bestTeamPointsPerGame}/5</p>
-            <p>Best points per game team = {bestTeamPointsPerGameTeam}</p>
-            <p>Best agg per game = {bestTeamAggPerGame}/21</p>
-            <p>Best agg per game team = {bestTeamAggPerGameTeam}</p>
-            <p>Fewest points conceded per game = {fewestPointsConcededPerGame}/5</p>
-            <p>Fewest points conceded per game team = {fewestPointsConcededPerGameTeam}</p>
-            <p>Fewest aggregated points conceded per game = {fewestAggConcededPerGame}/21</p>
-            <p>Fewest aggregated points conceded per game team = {fewestAggConcededPerGameTeam}</p>
+            <p>Best win percentage = {bestWinPercentage.toFixed(0)}%</p>
+            <p>Best win percentage Team = {capitalizeText(bestWinPercentageTeam)}</p>
+            <p>Best points per game = {bestTeamPointsPerGame.toFixed(1)}/5</p>
+            <p>Best points per game team = {capitalizeText(bestTeamPointsPerGameTeam)}</p>
+            <p>Best agg per game = {bestTeamAggPerGame.toFixed(1)}/21</p>
+            <p>Best agg per game team = {capitalizeText(bestTeamAggPerGameTeam)}</p>
+            <p>Fewest points conceded per game = {fewestPointsConcededPerGame.toFixed(1)}/5</p>
+            <p>Fewest points conceded per game team = {capitalizeText(fewestPointsConcededPerGameTeam)}</p>
+            <p>Fewest aggregated points conceded per game = {lowestAggConcededPerGame.toFixed(1)}/21</p>
+            <p>Fewest aggregated points conceded per game team = {capitalizeText(lowestAggConcededPerGameTeam)}</p>
         </div>
     );
 }
