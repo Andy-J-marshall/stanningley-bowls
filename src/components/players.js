8592cbe12db0e049084873b8cd31969d0a019345
Andrew Marshall <andrew.marshall@skybettingandgaming.com>
2022-06-09 18:31:41 +0100

    Added a bit of structure to the player stats

--- src/components/players.js
+++ src/components/players.js
@@ -33,6 +33,7 @@ function Players(props) {
             beatenBy: capitalizeText(beatenBy),
             beatenOpponents: capitalizeText(beatenOpponents),
             dayPlayed: capitalizeText(dayPlayed),
+            pairsGames: pairLosses + pairWins,
             pairLosses,
             pairWins,
             pairsPartners: capitalizeText(pairsPartners),
@@ -48,20 +49,37 @@ function Players(props) {
                 key={index}>
                 <div>
                     <h3>{capitalizeText([name])}</h3>
-                    {/* TODO display days played in nicer way */}
-                    <p>Games played = {data.gamesPlayed} ({data.dayPlayed})</p>
-                    <p>Games Won = {data.totalWins} ({data.homeWins} home, {data.awayWins} away, {data.pairWins} pairs)</p>
-                    <p>Games Lost = {data.totalLosses} ({data.homeLosses} home, {data.awayLosses} away, {data.pairLosses} pairs)</p>
-                    {/* TODO display pairsPartners in nicer way */}
-                    <p><b>Opponents:</b> Beat = {data.beatenOpponents}. Lost to = {data.beatenBy}</p>
-                    {data.pairsPartners.length > 0 && <p>Pairs partners = {data.pairsPartners}</p>}
-                    <p>Total points scored = {data.totalScore} / {data.gamesPlayed * 5}</p>
-                    <p>Total points conceded = {data.totalScoreAgainst} / {data.gamesPlayed * 5}</p>
-                    <p>Total aggregate = {data.totalAgg} / {data.gamesPlayed * 21}</p>
-                    <p>Total aggregate conceded = {data.totalAggAgainst} / {data.gamesPlayed * 21}</p>
-                    <p>Combined averages = {data.average.toFixed(2)}</p>
-                    <p>Combined average score = {data.averageScore.toFixed(2)} / 5</p>
-                    <p>Combined average score against = {data.averageScoreAgainst.toFixed(2)} / 5</p>
+                    {data.gamesPlayed === 0 && <p>No games played</p>}
+                    {data.gamesPlayed > 0 && <div>
+                        <h5>Games</h5>
+                        {/* TODO display days played in nicer way */}
+                        <p>{data.gamesPlayed} games played</p>
+                        {data.totalWins > 0 && <p>{data.totalWins} wins ({data.homeWins} home, {data.awayWins} away)</p>}
+                        {data.totalLosses > 0 && <p>{data.totalLosses} losses ({data.homeLosses} home, {data.awayLosses} away)</p>}
+                        {<p>Days played = {data.dayPlayed}</p>}
+
+                        {data.pairsGames > 0 && <div>
+                            <h5>Pairs</h5>
+                            {/* TODO display pairsPartners in nicer way */}
+                            <p>{data.pairsGames} pairs games played</p>
+                            <p>{data.pairWins} pairs wins</p>
+                            <p>{data.pairLosses} pairs losses</p>
+                            {data.pairsPartners.length > 0 && <p>Pairs partners = {data.pairsPartners}</p>}
+                        </div>}
+
+                        <h5>Opponents</h5>
+                        {data.beatenOpponents.length > 0 && <p>Beaten opponents = {data.beatenOpponents}</p>}
+                        {data.beatenBy.length > 0 && <p>Defeated by = {data.beatenBy}</p>}
+
+                        <h5>Averages</h5>
+                        <p>Total points scored = {data.totalScore} / {data.gamesPlayed * 5}</p>
+                        <p>Total points conceded = {data.totalScoreAgainst} / {data.gamesPlayed * 5}</p>
+                        <p>Total aggregate scored = {data.totalAgg} / {data.gamesPlayed * 21}</p>
+                        <p>Total aggregate conceded = {data.totalAggAgainst} / {data.gamesPlayed * 21}</p>
+                        <p>Average = {data.average.toFixed(2)}</p>
+                        <p>Combined average score = {data.averageScore.toFixed(2)} / 5</p>
+                        <p>Combined average score against = {data.averageScoreAgainst.toFixed(2)} / 5</p>
+                    </div>}
                 </div>
             </ListGroup.Item>
         </div>
