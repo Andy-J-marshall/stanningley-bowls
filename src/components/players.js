30a10e5a2502f1599b2026b4e0895bc8b6ea899f
Andrew Marshall <andrew.marshall@skybettingandgaming.com>
2022-06-12 00:51:03 +0100

    Refactored variables in players file

--- src/components/players.js
+++ src/components/players.js
@@ -7,42 +7,23 @@ function Players(props) {
     const player = props.player;
     const playersStats = props.playersStats;
     const name = props.name;
-    const data = returnPlayerData(player);
-    const daysPlayedCount = calculateDaysPlayedCount(data.dayPlayed);
-    const pairsPartnersCount = calculatePairsPartnersCount(data.pairsPartners)
 
-    // TODO handle plurals
-    // TODO refactor this
-    function returnPlayerData(player) {
-        const playerData = playersStats[player];
-        const {
-            totalAgg, totalAggAgainst, totalScore, totalScoreAgainst, awayLosses, homeLosses, homeWins, awayWins, beatenBy, beatenOpponents, dayPlayed, pairLosses, pairWins, pairsPartners,
-        } = playerData;
-        const returnObj = {
-            totalAgg,
-            totalAggAgainst,
-            totalScore,
-            totalScoreAgainst,
-            average: (totalAgg - totalAggAgainst) / (awayWins + homeWins + homeLosses + awayLosses),
-            averageScore: (totalScore) / (awayWins + homeWins + homeLosses + awayLosses),
-            averageScoreAgainst: (totalScoreAgainst) / (awayWins + homeWins + homeLosses + awayLosses),
-            awayLosses,
-            homeLosses,
-            totalLosses: awayLosses + homeLosses,
-            homeWins,
-            awayWins,
-            totalWins: awayWins + homeWins,
-            gamesPlayed: awayWins + homeWins + homeLosses + awayLosses,
-            beatenBy: capitalizeText(beatenBy),
-            beatenOpponents: capitalizeText(beatenOpponents),
-            dayPlayed: dayPlayed,
-            pairsGames: pairLosses + pairWins,
-            pairLosses,
-            pairWins,
-            pairsPartners,
-        }
-        return returnObj;
-    }
+    const playerData = playersStats[player];
+    let {
+        totalAgg, totalAggAgainst, totalScore, totalScoreAgainst, awayLosses, homeLosses, homeWins, awayWins, beatenBy, beatenOpponents, dayPlayed, pairLosses, pairWins, pairsPartners,
+    } = playerData;
+    const average = (totalAgg - totalAggAgainst) / (awayWins + homeWins + homeLosses + awayLosses);
+    const averageScore = (totalScore) / (awayWins + homeWins + homeLosses + awayLosses);
+    const averageScoreAgainst = (totalScoreAgainst) / (awayWins + homeWins + homeLosses + awayLosses);
+    const totalLosses = awayLosses + homeLosses;
+    const totalWins = awayWins + homeWins;
+    const gamesPlayed = awayWins + homeWins + homeLosses + awayLosses;
+    beatenBy = capitalizeText(beatenBy);
+    beatenOpponents = capitalizeText(beatenOpponents);
+    const pairsGames = pairLosses + pairWins;
+
+    const daysPlayedCount = calculateDaysPlayedCount(dayPlayed);
+    const pairsPartnersCount = calculatePairsPartnersCount(pairsPartners)
 
     function calculatePairsPartnersCount(allPairsPartners) {
         const uniquePartners = allPairsPartners.filter((partner, index) => {
@@ -77,51 +58,52 @@ function Players(props) {
         return returnObj.filter(day => day.gamesPlayed > 0);
     }
 
+    // TODO handle plurals
     return (
         <div>
             < ListGroup.Item key={index}>
                 <div>
                     <h3>{capitalizeText([name])}</h3>
-                    {data.gamesPlayed === 0 && <p>No games played</p>}
-                    {data.gamesPlayed > 0 && <div>
+                    {gamesPlayed === 0 && <p>No games played</p>}
+                    {gamesPlayed > 0 && <div>
                         <h5>Games</h5>
-                        <p>{data.gamesPlayed} games played in total</p>
+                        <p>{gamesPlayed} games played in total</p>
                         {daysPlayedCount.map(day => {
                             const key = Math.floor(Math.random() * 100000 + index);
                             return <p key={key}>{day.gamesPlayed} games played on {day.day}</p>
                         })}
-                        {data.pairsGames > 0 && <p>{data.pairsGames} pairs games played</p>}
+                        {pairsGames > 0 && <p>{pairsGames} pairs games played</p>}
 
                         <h5>Results</h5>
-                        {data.totalWins > 0 && <p>{data.totalWins} wins ({data.homeWins} home, {data.awayWins} away)</p>}
-                        {data.totalLosses > 0 && <p>{data.totalLosses} losses ({data.homeLosses} home, {data.awayLosses} away)</p>}
-                        <p>{(data.totalWins / data.gamesPlayed * 100).toFixed(0)}% win percentage</p>
+                        {totalWins > 0 && <p>{totalWins} wins ({homeWins} home, {awayWins} away)</p>}
+                        {totalLosses > 0 && <p>{totalLosses} losses ({homeLosses} home, {awayLosses} away)</p>}
+                        <p>{(totalWins / gamesPlayed * 100).toFixed(0)}% win percentage</p>
 
                         {/* TODO add more detail on pairs partners wins/losses  */}
-                        {data.pairsGames > 0 && <div>
+                        {pairsGames > 0 && <div>
                             <h5>Pairs</h5>
-                            <p>{data.pairsGames} pairs games played</p>
-                            <p>{data.pairWins} pairs wins</p>
-                            <p>{data.pairLosses} pairs losses</p>
-                            {data.pairsPartners.length > 0 && Object.keys(pairsPartnersCount).map(partner => {
+                            <p>{pairsGames} pairs games played</p>
+                            <p>{pairWins} pairs wins</p>
+                            <p>{pairLosses} pairs losses</p>
+                            {pairsPartners.length > 0 && Object.keys(pairsPartnersCount).map(partner => {
                                 const key = Math.floor(Math.random() * 100000 + index);
                                 return <p key={key}>{pairsPartnersCount[partner].timesPaired} games with {partner}</p>
                             })}
                         </div>}
 
                         <h5>Opponents</h5>
-                        {data.beatenOpponents.length > 0 && <p>Beaten opponents = {data.beatenOpponents}</p>}
-                        {data.beatenBy.length > 0 && <p>Defeated by = {data.beatenBy}</p>}
+                        {beatenOpponents.length > 0 && <p>Beaten opponents = {beatenOpponents}</p>}
+                        {beatenBy.length > 0 && <p>Defeated by = {beatenBy}</p>}
 
                         <h5>Averages</h5>
                         {/* TODO split in home and away? */}
-                        <p>Total points scored = {data.totalScore} / {data.gamesPlayed * 5}</p>
-                        <p>Total points conceded = {data.totalScoreAgainst} / {data.gamesPlayed * 5}</p>
-                        <p>Total aggregate scored = {data.totalAgg} / {data.gamesPlayed * 21}</p>
-                        <p>Total aggregate conceded = {data.totalAggAgainst} / {data.gamesPlayed * 21}</p>
-                        <p>Average = {data.average.toFixed(2)}</p>
-                        <p>Average score = {data.averageScore.toFixed(2)} / 5</p>
-                        <p>Average opponents score = {data.averageScoreAgainst.toFixed(2)} / 5</p>
+                        <p>Total points scored = {totalScore} / {gamesPlayed * 5}</p>
+                        <p>Total points conceded = {totalScoreAgainst} / {gamesPlayed * 5}</p>
+                        <p>Total aggregate scored = {totalAgg} / {gamesPlayed * 21}</p>
+                        <p>Total aggregate conceded = {totalAggAgainst} / {gamesPlayed * 21}</p>
+                        <p>Average = {average.toFixed(2)}</p>
+                        <p>Average score = {averageScore.toFixed(2)} / 5</p>
+                        <p>Average opponents score = {averageScoreAgainst.toFixed(2)} / 5</p>
                     </div>}
                 </div>
             </ListGroup.Item>
