317ddbda92b4bf8c45f116643acb4442eb4203a2
Andrew Marshall <andrew.marshall@skybettingandgaming.com>
2022-06-13 00:10:42 +0100

    Added more home and away specific stats

--- src/components/players.js
+++ src/components/players.js
@@ -8,22 +8,27 @@ function Players(props) {
     const playersStats = props.playersStats;
     const name = props.name;
 
-    // "totalPairsAgg": 0,
-    // "totalPairsAggAgainst": 0,
-    // "winningPairsPartners": [],
-    // "losingPairsPartners": []
-
     const playerData = playersStats[player];
     let {
-        totalAgg, totalAggAgainst, totalPairsAgg, totalPairsAggAgainst, totalScore, totalScoreAgainst, awayLosses, homeLosses, homeWins,
-        awayWins, winningPairsPartners, losingPairsPartners, beatenBy, beatenOpponents, dayPlayed, pairLosses, pairWins, pairsPartners,
+        totalAgg, totalAggAgainst, totalPairsAgg, totalPairsAggAgainst, totalScore, totalScoreAgainst,
+        awayLosses, homeLosses, homeWins, awayWins, winningPairsPartners, losingPairsPartners, beatenBy,
+        beatenOpponents, dayPlayed, pairLosses, pairWins, pairsPartners, totalHomeAgg, totalHomeAggAgainst,
+        totalAwayAgg, totalAwayAggAgainst, totalHomeScore, totalHomeScoreAgainst, totalAwayScore, totalAwayScoreAgainst,
     } = playerData;
-    const average = (totalAgg - totalAggAgainst) / (awayWins + homeWins + homeLosses + awayLosses);
-    const averageScore = (totalScore) / (awayWins + homeWins + homeLosses + awayLosses);
-    const averageScoreAgainst = (totalScoreAgainst) / (awayWins + homeWins + homeLosses + awayLosses);
     const totalLosses = awayLosses + homeLosses;
     const totalWins = awayWins + homeWins;
-    const gamesPlayed = awayWins + homeWins + homeLosses + awayLosses;
+    const gamesPlayed = totalLosses + totalWins;
+    const homeGamesPlayed = homeWins + homeLosses;
+    const awayGamesPlayed = awayWins + awayLosses;
+    const average = (totalAgg - totalAggAgainst) / gamesPlayed;
+    const homeAverage = (totalHomeAgg - totalHomeAggAgainst) / (homeGamesPlayed);
+    const awayAverage = (totalAwayAgg - totalAwayAggAgainst) / (awayGamesPlayed);
+    const averageScore = (totalScore) / gamesPlayed;
+    const averageScoreAgainst = (totalScoreAgainst) / gamesPlayed;
+    const homeAverageScore = (totalHomeScore) / homeGamesPlayed;
+    const homeAverageScoreAgainst = (totalHomeScoreAgainst) / homeGamesPlayed;
+    const awayAverageScore = (totalAwayScore) / awayGamesPlayed;
+    const awayAverageScoreAgainst = (totalAwayScoreAgainst) / awayGamesPlayed;
     beatenBy = capitalizeText(beatenBy);
     beatenOpponents = capitalizeText(beatenOpponents);
     const pairsGames = pairLosses + pairWins;
@@ -74,7 +79,7 @@ function Players(props) {
                     <h3>{capitalizeText([name])}</h3>
                     {gamesPlayed === 0 && <p>No games played</p>}
                     {gamesPlayed > 0 && <div>
-                        <h5>Games</h5>
+                        <h4>Games</h4>
                         <p>{gamesPlayed} games played in total</p>
                         {daysPlayedCount.map(day => {
                             const key = Math.floor(Math.random() * 100000 + index);
@@ -82,13 +87,13 @@ function Players(props) {
                         })}
                         {pairsGames > 0 && <p>{pairsGames} pairs games played</p>}
 
-                        <h5>Results</h5>
+                        <h4>Results</h4>
                         {totalWins > 0 && <p>{totalWins} wins ({homeWins} home, {awayWins} away)</p>}
                         {totalLosses > 0 && <p>{totalLosses} losses ({homeLosses} home, {awayLosses} away)</p>}
                         <p>{(totalWins / gamesPlayed * 100).toFixed(0)}% win percentage</p>
 
                         {pairsGames > 0 && <div>
-                            <h5>Pairs</h5>
+                            <h4>Pairs</h4>
                             <p>{pairsGames} pairs games played</p>
                             <p>{pairWins} pairs wins</p>
                             <p>{pairLosses} pairs losses</p>
@@ -96,7 +101,6 @@ function Players(props) {
                                 const key = Math.floor(Math.random() * 100000 + index);
                                 return <p key={key}>{pairsPartnersCount[partner].timesPaired} games played with {partner}</p>
                             })}
-                            {/* TODO pairs losses partners not working quite right e.g. Alyssa */}
                             {pairWins > 0 && Object.keys(pairsPartnersCountWins).map(partner => {
                                 const key = Math.floor(Math.random() * 100000 + index);
                                 return <p key={key}>{pairsPartnersCountWins[partner].timesPaired} games won with {partner}</p>
@@ -107,19 +111,40 @@ function Players(props) {
                             })}
                         </div>}
 
-                        <h5>Opponents</h5>
+                        <h4>Opponents</h4>
                         {beatenOpponents.length > 0 && <p>Beaten opponents = {beatenOpponents}</p>}
                         {beatenBy.length > 0 && <p>Defeated by = {beatenBy}</p>}
 
-                        <h5>Averages</h5>
-                        {/* TODO split in home and away? */}
-                        <p>Total points scored = {totalScore} / {gamesPlayed * 5}</p>
-                        <p>Total points conceded = {totalScoreAgainst} / {gamesPlayed * 5}</p>
-                        <p>Total aggregate scored = {totalAgg} / {gamesPlayed * 21}</p>
-                        <p>Total aggregate conceded = {totalAggAgainst} / {gamesPlayed * 21}</p>
+                        <h4>Averages</h4>
                         <p>Average = {average.toFixed(2)}</p>
+                        <p>Home average = {homeAverage.toFixed(2)}</p>
+                        <p>Away average = {awayAverage.toFixed(2)}</p>
+
                         <p>Average score = {averageScore.toFixed(2)} / 5</p>
                         <p>Average opponents score = {averageScoreAgainst.toFixed(2)} / 5</p>
+                        <p>Average home score = {homeAverageScore.toFixed(2)} / 5</p>
+                        <p>Average home opponents score = {homeAverageScoreAgainst.toFixed(2)} / 5</p>
+                        <p>Average away score = {awayAverageScore.toFixed(2)} / 5</p>
+                        <p>Average away opponents score = {awayAverageScoreAgainst.toFixed(2)} / 5</p>
+
+                        <h4>Aggregates</h4>
+                        <p><b>Points</b></p>
+                        <p>Total aggregate scored = {totalAgg} / {gamesPlayed * 21}</p>
+                        <p>Total aggregate conceded = {totalAggAgainst} / {gamesPlayed * 21}</p>
+                        <p>Total home aggregate scored = {totalHomeAgg} / {homeGamesPlayed * 21}</p>
+                        <p>Total home aggregate conceded = {totalHomeAggAgainst} / {homeGamesPlayed * 21}</p>
+                        <p>Total away aggregate scored = {totalAwayAgg} / {awayGamesPlayed * 21}</p>
+                        <p>Total away aggregate conceded = {totalAwayAggAgainst} / {awayGamesPlayed * 21}</p>
+                        {pairsGames > 0 && <p>Total pairs aggregate scored = {totalPairsAgg} / {pairsGames * 21}</p>}
+                        {pairsGames > 0 && <p>Total pairs aggregate conceded = {totalPairsAggAgainst} / {pairsGames * 21}</p>}
+
+                        <p><b>Team Points</b></p>
+                        <p>Total points scored = {totalScore} / {gamesPlayed * 5}</p>
+                        <p>Total points conceded = {totalScoreAgainst} / {gamesPlayed * 5}</p>
+                        <p>Total home points scored = {totalHomeScore} / {homeGamesPlayed * 5}</p>
+                        <p>Total home points conceded = {totalHomeScoreAgainst} / {homeGamesPlayed * 5}</p>
+                        <p>Total away points scored = {totalAwayScore} / {awayGamesPlayed * 5}</p>
+                        <p>Total away points conceded = {totalAwayScoreAgainst} / {awayGamesPlayed * 5}</p>
                     </div>}
                 </div>
             </ListGroup.Item>
