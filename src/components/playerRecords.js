d3fb97ba7b910e4bb71081f6f496cb3dae2d6d0c
Andrew Marshall <andrew.marshall@skybettingandgaming.com>
2022-06-17 11:50:25 +0100

    Added more player records

--- src/components/playerRecords.js
+++ src/components/playerRecords.js
@@ -20,12 +20,16 @@ function PlayerRecords(props) {
     let bestWinPerc = 0;
     let bestAveragePlayer = [];
     let bestAverage = 0;
+    let bestScorePlayer = [];
+    let bestScore = 0;
 
     players.forEach(player => {
         const p = playersStats[player];
         const totalWins = p.awayWins + p.homeWins + p.cupWins;
         const totalLosses = p.awayLosses + p.homeLosses + p.cupLosses;
         const totalGames = totalWins + totalLosses;
+        
+        const playedMinGames = totalGames >= 6 ? true : false;
         // TODO the python script is picking up players matches for other teams e.g. Mario/ Shirley
         if (totalGames >= mostGames) {
             if (totalGames > mostGames) {
@@ -42,7 +46,7 @@ function PlayerRecords(props) {
             mostWinsPlayer.push(player);
         }
         const winPerc = totalWins / totalGames * 100;
-        if (winPerc >= bestWinPerc && totalGames >= 6) {
+        if (winPerc >= bestWinPerc && playedMinGames) {
             if (winPerc > bestWinPerc) {
                 bestWinPercPlayer = [];
                 bestWinPerc = winPerc;
@@ -50,16 +54,22 @@ function PlayerRecords(props) {
             bestWinPercPlayer.push(player);
         }
         const average = (p.totalAgg - p.totalAggAgainst) / totalGames;
-        if (average >= bestAverage && totalGames >= 6) {
+        if (average >= bestAverage && playedMinGames) {
             if (average > bestAverage) {
                 bestAveragePlayer = [];
                 bestAverage = average;
             }
             bestAveragePlayer.push(player);
         }
-        // TODO 
-        // most points scored? Most team points?
-        // Split home and away? Day?
+
+        const score = p.totalScore / totalGames;
+        if (score >= bestScore && playedMinGames) {
+            if (score > bestScore) {
+                bestScorePlayer = [];
+                bestScore = score;
+            }
+            bestScorePlayer.push(player);
+        }
     })
 
     return (
@@ -67,8 +77,9 @@ function PlayerRecords(props) {
             <h2>Player Records</h2>
             <p>Most games played = {capitalizeText(mostGamesPlayer)} ({mostGames})</p>
             <p>Most wins = {capitalizeText(mostWinsPlayer)} ({mostWins})</p>
-            <p>Best win percentage = {capitalizeText(bestWinPercPlayer)} ({bestWinPerc}% - min 6 games)</p>
-            <p>Best average = {capitalizeText(bestAveragePlayer)} ({bestAverage} - min 6 games)</p>
+            <p>Best win percentage = {capitalizeText(bestWinPercPlayer)} ({bestWinPerc.toFixed(0)}% - min 6 games)</p>
+            <p>Best average = {capitalizeText(bestAveragePlayer)} ({bestAverage.toFixed(2)} - min 6 games)</p>
+            <p>Best team score per game = {capitalizeText(bestScorePlayer)} ({bestScore.toFixed(2)} - min 6 games)</p>
         </div>
     );
 }
