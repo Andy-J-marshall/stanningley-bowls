707fe05ac6586caaa13dc3879429af295ca161ff
Andrew Marshall <andrew.marshall@skybettingandgaming.com>
2022-06-17 13:42:35 +0100

    Added more toggles to display stats

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import TeamStats from './teamStats';
 import PlayerStats from './playerStats';
 
 function Stats() {
+    const [showStats, setShowStats] = useState(false);
+    const [showTeamStats, setShowTeamStats] = useState(false);
+
+    // TODO decide how to display this information. Maybe records together? Use a nav bar?
+    // TODO what info should I default to?
+
+    function displayPlayerStats() {
+        if (showStats) {
+            setShowStats(false);
+        } else {
+            setShowStats(true);
+        }
+    }
+
+
+    function displayTeamStats() {
+        if (showStats) {
+            setShowTeamStats(false);
+        } else {
+            setShowTeamStats(true);
+        }
+    }
+
+
     return (
         <div>
-            <PlayerStats />
-            <TeamStats />
+            <Button variant='light' size='lg' onClick={displayPlayerStats}>{'Player Stats'}</Button>
+            <Button variant='light' size='lg' onClick={displayTeamStats}>{'Team Stats'}</Button>
+            <br />
+            <br />
+            {showStats && <PlayerStats />}
+            {showTeamStats && <TeamStats />}
         </div>
     );
 }
