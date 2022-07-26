f085f1420a94ad365f6fca1ef6e7b175eb71128a
Andrew Marshall <andrew.marshall@amido.com>
2022-07-26 13:20:45 +0100

    Copying the Monday league report

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,51 @@
-import { test, expect } from '@playwright/test';
+import { test, chromium } from '@playwright/test';
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Monday Stats', async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  const dateFromValue = '17496';
+  const dateToValue = '18784';
+  await page.goto('https://bowlsnet.uk/Leeds/MonComb');
 
-  // create a locator
-  const getStarted = page.locator('text=Get Started');
+  // TODO make this optional?
+  await page.frameLocator('#x-Dframe').locator('#x-DlgI > input').click();
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  await page
+    .frameLocator('#x-Pframe')
+    .locator(
+      '#x-Menu > .menuSect:nth-child(2) > .menu-btn[onclick*="MenuPage(\'Info\')"]'
+    )
+    .click();
 
-  // Click the get started link.
-  await getStarted.click();
+  await page
+    .frameLocator('#x-Pframe')
+    .locator('#bbar > .bbar-tab[onclick*="ShowSubPage(4)"]')
+    .click();
+  await page.frameLocator('#x-Pframe').locator('#r1').click();
+  await page.frameLocator('#x-Pframe').locator('#oTab1').click();
+  await page.frameLocator('#x-Pframe').locator('#oRes').click();
+  await page.frameLocator('#x-Pframe').locator('#oResFull').click();
+  await page
+    .frameLocator('#x-Pframe')
+    .locator('#x-DlgF > .dGrp > select[name=oResF]')
+    .selectOption(dateFromValue);
+  await page
+    .frameLocator('#x-Pframe')
+    .locator('#x-DlgF > .dGrp > select[name=oResT]')
+    .selectOption(dateToValue);
+  const [newPage] = await Promise.all([
+    context.waitForEvent('page'),
+    await page.frameLocator('#x-Pframe').locator('#dRBtn').click(),
+  ]);
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  await newPage.bringToFront();
+  const modifier = 'Meta';
+  await newPage.focus('body');
+  await newPage.keyboard.press(`${modifier}+KeyA`);
+  await newPage.keyboard.press(`${modifier}+KeyC`);
+
+  // await page.keyboard.press(`${modifier}+KeyV`);
+  // await page.keyboard.press(`${modifier}+KeyV`);
 });
