aa5f69752b6cf8219e4dca7138d49e4ea0e3aa48
Andrew Marshall <andrew.marshall@amido.com>
2022-07-26 16:23:37 +0100

    automated tests now create local files with the data from bowlsnet

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,51 +1,95 @@
 import { test, chromium } from '@playwright/test';
+import fs from 'fs';
 
-test('Monday Stats', async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
+let browser;
+let context;
+let page;
 
-  const dateFromValue = '17496';
-  const dateToValue = '18784';
-  await page.goto('https://bowlsnet.uk/Leeds/MonComb');
+const teams = [
+  {
+    day: 'Monday',
+    url: 'https://bowlsnet.uk/Leeds/MonComb',
+    dateFromValue: '17496',
+    dateToValue: '18784',
+  },
+  {
+    day: 'Tuesday',
+    url: 'https://bowlsnet.uk/LeedsParkVets/Tue',
+    dateFromValue: '17560',
+    dateToValue: '18848',
+  },
+  {
+    day: 'Thursday',
+    url: 'https://bowlsnet.uk/LeedsParkVets/Thu',
+    dateFromValue: '17576',
+    dateToValue: '18752',
+  },
+  {
+    day: 'Saturday',
+    url: 'https://bowlsnet.uk/Leeds/Sat',
+    dateFromValue: '17424',
+    dateToValue: '18880',
+  },
+];
 
-  // TODO make this optional?
-  await page.frameLocator('#x-Dframe').locator('#x-DlgI > input').click();
+for (const team of teams) {
+  test.beforeEach(async () => {
+    browser = await chromium.launch();
+    context = await browser.newContext();
+    page = await context.newPage();
+  });
 
-  await page
-    .frameLocator('#x-Pframe')
-    .locator(
-      '#x-Menu > .menuSect:nth-child(2) > .menu-btn[onclick*="MenuPage(\'Info\')"]'
-    )
-    .click();
+  test(`${team.day} Stats`, async () => {
+    const day = team.day;
+    const filePath = `./files/htmlFiles/${day}.html`;
+    const dateFromValue = team.dateFromValue;
+    const dateToValue = team.dateToValue;
+    const url = team.url;
+    await page.goto(url);
 
-  await page
-    .frameLocator('#x-Pframe')
-    .locator('#bbar > .bbar-tab[onclick*="ShowSubPage(4)"]')
-    .click();
-  await page.frameLocator('#x-Pframe').locator('#r1').click();
-  await page.frameLocator('#x-Pframe').locator('#oTab1').click();
-  await page.frameLocator('#x-Pframe').locator('#oRes').click();
-  await page.frameLocator('#x-Pframe').locator('#oResFull').click();
-  await page
-    .frameLocator('#x-Pframe')
-    .locator('#x-DlgF > .dGrp > select[name=oResF]')
-    .selectOption(dateFromValue);
-  await page
-    .frameLocator('#x-Pframe')
-    .locator('#x-DlgF > .dGrp > select[name=oResT]')
-    .selectOption(dateToValue);
-  const [newPage] = await Promise.all([
-    context.waitForEvent('page'),
-    await page.frameLocator('#x-Pframe').locator('#dRBtn').click(),
-  ]);
+    if ((await page.$('#x-Dframe')) !== null) {
+      await page.frameLocator('#x-Dframe').locator('#x-DlgI > input').click();
+    } else {
+      console.log('No pop up clicked');
+    }
 
-  await newPage.bringToFront();
-  const modifier = 'Meta';
-  await newPage.focus('body');
-  await newPage.keyboard.press(`${modifier}+KeyA`);
-  await newPage.keyboard.press(`${modifier}+KeyC`);
+    await page
+      .frameLocator('#x-Pframe')
+      .locator(
+        '#x-Menu > .menuSect:nth-child(2) > .menu-btn[onclick*="MenuPage(\'Info\')"]'
+      )
+      .click();
 
-  // await page.keyboard.press(`${modifier}+KeyV`);
-  // await page.keyboard.press(`${modifier}+KeyV`);
-});
+    await page
+      .frameLocator('#x-Pframe')
+      .locator('#bbar > .bbar-tab[onclick*="ShowSubPage(4)"]')
+      .click();
+    await page.frameLocator('#x-Pframe').locator('#r1').click();
+    await page.frameLocator('#x-Pframe').locator('#oTab1').click();
+    await page.frameLocator('#x-Pframe').locator('#oRes').click();
+    await page.frameLocator('#x-Pframe').locator('#oResFull').click();
+    await page
+      .frameLocator('#x-Pframe')
+      .locator('#x-DlgF > .dGrp > select[name=oResF]')
+      .selectOption(dateFromValue);
+    await page
+      .frameLocator('#x-Pframe')
+      .locator('#x-DlgF > .dGrp > select[name=oResT]')
+      .selectOption(dateToValue);
+    const [newPage] = await Promise.all([
+      context.waitForEvent('page'),
+      await page.frameLocator('#x-Pframe').locator('#dRBtn').click(),
+    ]);
+
+    await newPage.bringToFront();
+    const value = await newPage.$eval('body', (el) => el.innerHTML);
+    const html = `<html><body>${value}</body></html>`;
+
+    fs.writeFile(filePath, html, (err) => {
+      if (err) {
+        console.log(`Failed to update ${day} html file`);
+        console.error(err);
+      }
+    });
+  });
+}
