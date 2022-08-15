50d4195ed9428904b1f7d2b8ba92e5add7b00ff9
Andrew Marshall <andrew.marshall@amido.com>
2022-08-15 12:03:08 +0100

    Improved playwright config files

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,45 +2,30 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
   testDir: './tests',
-  /* Maximum time one test can run for. */
   timeout: 20 * 1000,
   expect: {
-    /* Maximum time expect() should wait for the condition to be met.*/
     timeout: 5000,
   },
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  // retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 5 : undefined,
+  retries: 0,
+  workers: process.env.CI ? 5 : 8,
   maxFailures: 10,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  outputDir: 'test-results/',
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:3000',
     timeout: 60 * 1000,
-    // TODO add back in? reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env.CI,
   },
   use: {
     baseURL: process.env.CI ? 'https://stanningleybowlsclub.co.uk' : 'http://localhost:3000',
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
   },
@@ -96,9 +81,6 @@ const config: PlaywrightTestConfig = {
     //   },
     // },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: 'test-results/',
 };
 
 export default config;
