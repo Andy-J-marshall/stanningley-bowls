b707d92e80e8e1c925e034d1f886e667790a6b1a
Andrew Marshall <andrew.marshall@amido.com>
2023-02-23 15:35:01 +0000

    Added calendar for fixtures

--- src/components/fixtures.js
+++ src/components/fixtures.js
@@ -1,107 +1,23 @@
 import { useEffect } from 'react';
-import upcomingFixturesApril from '../images/fixtures/upcoming_fixtures_April.png';
-import upcomingFixturesMay from '../images/fixtures/upcoming_fixtures_May.png';
-import upcomingFixturesJune from '../images/fixtures/upcoming_fixtures_June.png';
-import upcomingFixturesJuly from '../images/fixtures/upcoming_fixtures_July.png';
-import upcomingFixturesAugust from '../images/fixtures/upcoming_fixtures_Aug.png';
-import upcomingFixturesSeptember from '../images/fixtures/upcoming_fixtures_Sept.png';
 
-// TODO get the fixtures from bowlsnet instead?
+// TODO Add Tuesday fixtures to calendar when available, and check there's been no updates
 
 function FixturesResults() {
     useEffect(() => {
         window.scrollTo(0, 0);
     });
 
-    const date = new Date();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    // TODO update
-    const yearFixturesAddedFor = 2022; // Will need to change this when fixtures added for future years
-
-    if (year === yearFixturesAddedFor && month <= 9) {
-        return (
-            <div id="fixture" className="center page-component">
-                <div>
-                    <hr style={{ margin: 0 }} />
-                    {4 >= month && (
-                        <div>
-                            <img
-                                id="upcoming-fixtures-april"
-                                src={upcomingFixturesApril}
-                                alt="upcoming fixtures for April"
-                                className="upcoming-fixtures"
-                            />
-                            <hr style={{ margin: 0 }} />
-                        </div>
-                    )}
-                    {5 >= month && (
-                        <div>
-                            <img
-                                id="upcoming-fixtures-may"
-                                src={upcomingFixturesMay}
-                                alt="upcoming fixtures for May"
-                                className="upcoming-fixtures"
-                            />
-                            <hr style={{ margin: 0 }} />
-                        </div>
-                    )}
-                    {6 >= month && (
-                        <div>
-                            <img
-                                id="upcoming-fixtures-june"
-                                src={upcomingFixturesJune}
-                                alt="upcoming fixtures for June"
-                                className="upcoming-fixtures"
-                            />
-                            <hr style={{ margin: 0 }} />
-                        </div>
-                    )}
-                    {7 >= month && (
-                        <div>
-                            <img
-                                id="upcoming-fixtures-july"
-                                src={upcomingFixturesJuly}
-                                alt="upcoming fixtures for July"
-                                className="upcoming-fixtures"
-                            />
-                            <hr style={{ margin: 0 }} />
-                        </div>
-                    )}
-                    {8 >= month && (
-                        <div>
-                            <img
-                                id="upcoming-fixtures-august"
-                                src={upcomingFixturesAugust}
-                                alt="upcoming fixtures for August"
-                                className="upcoming-fixtures"
-                            />
-                        </div>
-                    )}
-                    <hr style={{ margin: 0 }} />
-                    {9 >= month && (
-                        <div>
-                            <img
-                                id="upcoming-fixtures-september"
-                                src={upcomingFixturesSeptember}
-                                alt="upcoming fixtures for September"
-                                className="upcoming-fixtures"
-                            />
-                            <hr style={{ margin: 0 }} />
-                        </div>
-                    )}
-                </div>
-            </div>
-        );
-    } else {
-        return (
-            <div id="fixture" className="center page-component">
-                <h1>Fixtures</h1>
-                {month < 4 && <p>Fixtures are not currently available.</p>}
-                {month > 9 && <p>The season has finished</p>}
-            </div>
-        );
-    }
+    return (
+        <div id="fixture" className="center page-component">
+            <h1>Fixtures</h1>
+            <iframe
+                id="fixture-calendar"
+                src="https://calendar.google.com/calendar/embed?height=400&wkst=2&bgcolor=%23ffffff&ctz=UTC&title=Stanningley%20Park%20Fixtures%20And%20Events&showTz=0&showDate=1&showCalendars=0&ctz=Europe%2FLondon&showTabs=1&showPrint=0&showDate=0&showTitle=0&showNav=1&src=c3Rhbm5pbmdsZXlib3dsc2NsdWJAZ21haWwuY29t&color=%23039BE5"
+                width="95%"
+                height="550"
+            ></iframe>
+        </div>
+    );
 }
 
 export default FixturesResults;
