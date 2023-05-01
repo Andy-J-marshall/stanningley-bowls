9090e5094d5ce944cf2ec19129ec7d4f0069f102
Andrew Marshall <andrew.marshall@amido.com>
2023-05-01 16:42:37 +0100

    changed date format of fixture calendar to BST

--- src/components/fixtures.js
+++ src/components/fixtures.js
@@ -10,7 +10,7 @@ function FixturesResults() {
             <h1>Fixtures</h1>
             <iframe
                 id="fixture-calendar"
-                src="https://calendar.google.com/calendar/embed?height=400&wkst=2&bgcolor=%23ffffff&ctz=UTC&title=Stanningley%20Park%20Fixtures%20And%20Events&showTz=0&showDate=1&showCalendars=0&ctz=Europe%2FLondon&showTabs=1&showPrint=0&showDate=0&showTitle=0&showNav=1&src=c3Rhbm5pbmdsZXlib3dsc2NsdWJAZ21haWwuY29t&color=%23039BE5"
+                src="https://calendar.google.com/calendar/embed?height=400&wkst=2&bgcolor=%23ffffff&ctz=Europe&title=Stanningley%20Park%20Fixtures%20And%20Events&showTz=0&showDate=1&showCalendars=0&ctz=BritishSummerTime%2FLondon&showTabs=1&showPrint=0&showDate=0&showTitle=0&showNav=1&src=c3Rhbm5pbmdsZXlib3dsc2NsdWJAZ21haWwuY29t&color=%23039BE5"
                 width="95%"
                 height="550"
             ></iframe>
