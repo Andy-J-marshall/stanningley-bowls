f086b9fda31307058ec28458c1167b2bc422cf33
Andrew Marshall <andrew.marshall@amido.com>
2023-05-20 00:16:13 +0100

    added option to order table rows

--- src/components/playerStatSummary.js
+++ src/components/playerStatSummary.js
@@ -1,19 +1,116 @@
-import { Table } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Table } from 'react-bootstrap';
 import { capitalizeText } from '../helpers/utils';
 
 function PlayerStatSummary(props) {
-    const playerStats = props.playerStats;
+    let playerStats = props.playerStats;
     const displayPlayerStatsCallback = props.callback;
     const showSinglesOnlyBool = props.showSinglesOnly;
 
+    playerStats = playerStats.filter((player) => player.games > 0);
+
     let style;
     let href;
+
+    const [orderedPlayerStats, setOrderedPlayerStats] = useState(playerStats);
 
     function displayPlayer(event) {
         const playerName = event.target.innerHTML;
         if (displayPlayerStatsCallback) {
             displayPlayerStatsCallback(playerName);
         }
+    }
+
+    function orderByName() {
+        const playerOrder = [...playerStats].sort();
+
+        setOrderedPlayerStats(playerOrder);
+    }
+
+    function orderByGames() {
+        let gameOrder;
+
+        if (showSinglesOnlyBool) {
+            gameOrder = [...playerStats].sort((p1, p2) =>
+                p1.singleGames < p2.singleGames
+                    ? 1
+                    : p1.singleGames > p2.singleGames
+                    ? -1
+                    : 0
+            );
+        } else {
+            gameOrder = [...playerStats].sort((p1, p2) =>
+                p1.games < p2.games ? 1 : p1.games > p2.games ? -1 : 0
+            );
+        }
+
+        setOrderedPlayerStats(gameOrder);
+    }
+
+    function orderByWins() {
+        let winOrder;
+
+        if (showSinglesOnlyBool) {
+            winOrder = [...playerStats].sort((p1, p2) =>
+                p1.singlesWins < p2.singlesWins
+                    ? 1
+                    : p1.singlesWins > p2.singlesWins
+                    ? -1
+                    : 0
+            );
+        } else {
+            winOrder = [...playerStats].sort((p1, p2) =>
+                p1.wins < p2.wins ? 1 : p1.wins > p2.wins ? -1 : 0
+            );
+        }
+
+        setOrderedPlayerStats(winOrder);
+    }
+
+    function orderByWinPerc() {
+        let winPercOrder;
+
+        if (showSinglesOnlyBool) {
+            winPercOrder = [...playerStats].sort((p1, p2) =>
+                (p1.singlesWins / p1.singleGames) * 100 <
+                (p2.singlesWins / p2.singleGames) * 100
+                    ? 1
+                    : (p1.singlesWins / p1.singleGames) * 100 >
+                      (p2.singlesWins / p2.singleGames) * 100
+                    ? -1
+                    : 0
+            );
+        } else {
+            winPercOrder = [...playerStats].sort((p1, p2) =>
+                (p1.wins / p1.games) * 100 < (p2.wins / p2.games) * 100
+                    ? 1
+                    : (p1.wins / p1.games) * 100 > (p2.wins / p2.games) * 100
+                    ? -1
+                    : 0
+            );
+        }
+
+        setOrderedPlayerStats(winPercOrder);
+    }
+
+    function orderByAverage() {
+        let averageOrder;
+
+        if (showSinglesOnlyBool) {
+            averageOrder = [...playerStats].sort((p1, p2) =>
+                p1.singlesAverage < p2.singlesAverage
+                    ? 1
+                    : p1.singlesAverage > p2.singlesAverage
+                    ? -1
+                    : 0
+            );
+        } else {
+            averageOrder = [...playerStats].sort((p1, p2) =>
+                p1.average < p2.average ? 1 : p1.average > p2.average ? -1 : 0
+            );
+        }
+
+        setOrderedPlayerStats(averageOrder);
     }
 
     if (displayPlayerStatsCallback) {
@@ -30,21 +127,81 @@ function PlayerStatSummary(props) {
         href = null;
     }
 
-    if (playerStats) {
+    if (orderedPlayerStats) {
         return (
             <div id="player-stats-per-team">
                 <div className="center table" style={{ width: '97%' }}>
                     <Table striped bordered hover>
                         <thead>
                             <tr>
-                                <th>PLAYER</th>
-                                <th>GAMES</th>
-                                <th>WINS</th>
-                                <th>WIN %</th>
-                                <th>AVERAGE</th>
+                                <th>
+                                    <Button
+                                        id="order-by-win%-button"
+                                        variant="light"
+                                        onClick={orderByName}
+                                        style={{
+                                            backgroundColor: 'white',
+                                            border: 'none',
+                                        }}
+                                    >
+                                        PLAYER
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button
+                                        id="order-by-win%-button"
+                                        variant="light"
+                                        onClick={orderByGames}
+                                        style={{
+                                            backgroundColor: 'white',
+                                            border: 'none',
+                                        }}
+                                    >
+                                        GAMES
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button
+                                        id="order-by-wins-button"
+                                        variant="light"
+                                        onClick={orderByWins}
+                                        style={{
+                                            backgroundColor: 'white',
+                                            border: 'none',
+                                        }}
+                                    >
+                                        WINS
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button
+                                        id="order-by-win%-button"
+                                        variant="light"
+                                        onClick={orderByWinPerc}
+                                        style={{
+                                            backgroundColor: 'white',
+                                            border: 'none',
+                                        }}
+                                    >
+                                        WIN %
+                                    </Button>
+                                </th>
+                                <th>
+                                    <Button
+                                        id="order-by-win%-button"
+                                        variant="light"
+                                        onClick={orderByAverage}
+                                        style={{
+                                            backgroundColor: 'white',
+                                            border: 'none',
+                                        }}
+                                    >
+                                        AVERAGE
+                                    </Button>
+                                </th>
                             </tr>
                         </thead>
-                        {playerStats.map((player, key) => {
+                        {orderedPlayerStats.map((player, key) => {
                             let gamesPlayed = player.games;
                             let average = player.average;
                             let wins = player.wins;
