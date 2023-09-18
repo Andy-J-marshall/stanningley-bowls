a37d67d18237da61578a2b38845356af748e306a
Andrew Marshall <andrew.marshall@amido.com>
2023-09-19 00:28:05 +0100

    added sleep to allow pop to appear in playwright script, and finding date drop down options based on index rather than hardcoding

--- scripts/bowlsnet/getDataFromBowlsnet.spec.ts
+++ scripts/bowlsnet/getDataFromBowlsnet.spec.ts
@@ -5,97 +5,70 @@ let browser;
 let context;
 let page;
 
-// TODO possible to change the dateTo and dateFrom values to first/last ones in index?
+function sleep() {
+  return new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
 const teams = [
   {
     day: 'Monday Combined Leeds',
     url: '/Leeds-MonComb',
-    dateFromValue: '25680',
-    dateToValue: '26912',
   },
   {
     day: 'Tuesday Vets Leeds',
     url: '/LeedsParkVets-Tue',
-    dateFromValue: '25688',
-    dateToValue: '26920',
   },
   {
     day: 'Tuesday Leeds',
     url: '/Leeds-Tue',
-    dateFromValue: '25688',
-    dateToValue: '26920',
   },
   {
     day: 'Wednesday Half Holiday Leeds',
     url: '/Leeds-Wed',
-    dateFromValue: '25752',
-    dateToValue: '26808',
   },
   {
     day: 'Thursday Vets Leeds',
     url: '/LeedsParkVets-Thu',
-    dateFromValue: '25760',
-    dateToValue: '26872',
   },
   {
     day: 'Saturday Leeds',
     url: '/Leeds-Sat',
-    dateFromValue: '25664',
-    dateToValue: '26952',
   },
   {
     day: 'Monday AireDale & Wharfedale',
     url: '/AW-Mon',
-    dateFromValue: '25680',
-    dateToValue: '26848',
   },
   {
     day: 'Wednesday AireDale & Wharfedale',
     url: '/AW-WedSingles',
-    dateFromValue: '25640',
-    dateToValue: '27096',
   },
   {
     day: 'Tuesday AireDale & Wharfedale',
     url: '/AW-Vets',
-    dateFromValue: '25688',
-    dateToValue: '26920',
   },
   {
     day: 'Monday Bradford',
     url: '/Bradford-Mon',
-    dateFromValue: '25680',
-    dateToValue: '26792',
   },
   {
     day: 'Wednesday Half Holiday Bradford',
     url: '/Bradford-HalfHol',
-    dateFromValue: '25752',
-    dateToValue: '27040',
   },
   {
     day: 'Saturday Bradford',
     url: '/Bradford-Sat',
-    dateFromValue: '25472',
-    dateToValue: '27192',
   },
   {
     day: 'Wednesday Spen Valley',
     url: '/WestRiding',
-    dateFromValue: '25608',
-    dateToValue: '26816',
   },
   {
     day: 'Tuesday Mirfield',
     url: '/Mirfield',
-    dateFromValue: '25744',
-    dateToValue: '27016',
   },
   {
     day: 'Thursday Vets Bradford',
     url: '/BradfordVets',
-    dateFromValue: '25584',
-    dateToValue: '26872',
   },
 ];
 
@@ -110,28 +83,17 @@ for (const team of teams) {
     const day = team.day;
     const filePath = `./files/htmlFiles/${day}.html`;
     const url = team.url;
-    const dateFromValue = team.dateFromValue;
-    const dateToValue = team.dateToValue;
 
     await page.goto(url);
+    await sleep();
 
     const popUp = await page
       .frameLocator('#x-Dframe')
       .locator('#x-DlgI > .dlgBtn[value*="Close"]');
-    if (popUp) {
-      popUp.click();
-    } else {
-      console.log('No pop up clicked');
-    }
+    const popUpCount = await popUp.count();
 
-    // This is a temporary measure whilst the site update appears
-    const continueButton = await page
-      .frameLocator('#x-Pframe')
-      .locator('#fpTab .fpBtn');
-    if (popUp) {
-      continueButton.click();
-    } else {
-      console.log('No Continue button clicked');
+    if (popUpCount > 0) {
+      await popUp.click();
     }
 
     await page
@@ -157,11 +119,17 @@ for (const team of teams) {
     await page
       .frameLocator('#x-Pframe')
       .locator('#x-DlgF > .dGrp > select[name=oResF]')
-      .selectOption(dateFromValue);
+      .selectOption({ index: 1 });
+
+    const dateOptionCount = await page
+      .frameLocator('#x-Pframe')
+      .locator('#x-DlgF > .dGrp > select[name=oResT] > option')
+      .count();
+
     await page
       .frameLocator('#x-Pframe')
       .locator('#x-DlgF > .dGrp > select[name=oResT]')
-      .selectOption(dateToValue);
+      .selectOption({ index: dateOptionCount - 1 });
     await page
       .frameLocator('#x-Pframe')
       .locator('#x-DlgF [name="oResFull"]')
