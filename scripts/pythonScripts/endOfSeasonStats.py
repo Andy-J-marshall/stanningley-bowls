cd79629ce15b5038897b2c309e4b814e338ab26f
Andrew Marshall <andrew.marshall@ensono.com>
2023-12-15 20:44:29 +0000

    added nillings stat to script

--- scripts/pythonScripts/endOfSeasonStats.py
+++ scripts/pythonScripts/endOfSeasonStats.py
@@ -11,6 +11,8 @@ totalSinglesGames = 0
 totalPairsGames = 0
 singleFigureWinsInSingles = 0
 singleFigureWinsInPairs = 0
+nillingsInSingles = 0
+nillingsInPairs = 0
 
 for player in playerStats.keys():
 
@@ -21,19 +23,26 @@ for player in playerStats.keys():
 
         if "&" in result:
             totalPairsGames += 1
-            if opponentScore < 10 and opponentScore > 0:
+            if opponentScore < 10 and opponentScore >= 0:
                 singleFigureWinsInPairs +=1
+                if opponentScore == 0:
+                    nillingsInPairs += 1
         else:
             totalSinglesGames += 1
-            if opponentScore < 10 and opponentScore > 0:
+            if opponentScore < 10 and opponentScore >= 0:
                 singleFigureWinsInSingles +=1
+                if opponentScore == 0:
+                    nillingsInSingles += 1
 
 # Deduplicate pairs games
 totalPairsGames = int(totalPairsGames / 2)
 singleFigureWinsInPairs = int(singleFigureWinsInPairs / 2)
+nillingsInPairs = int(nillingsInPairs / 2)
 
 totalGames = totalPairsGames + totalSinglesGames
 singleFigureWinsTotal = singleFigureWinsInSingles + singleFigureWinsInPairs
+totalNillings = nillingsInSingles + nillingsInPairs
 
 print("total games played: " + str(totalGames))
 print("Total single figure winners: " + str(singleFigureWinsTotal))
+print("Total nillings: " + str(totalNillings))
