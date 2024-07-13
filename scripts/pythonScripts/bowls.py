44d297515fcb13cf2c465ec77555bf9e8c7ef9db
Andrew Marshall <andrew.marshall@ensono.com>
2024-07-13 01:11:25 +0100

    formatting

--- scripts/pythonScripts/bowls.py
+++ scripts/pythonScripts/bowls.py
@@ -42,15 +42,15 @@ def checkValidPlayerOnDay(playerName, row):
         previousRowValue = sheet['A' + str(row.row - i)]
         if previousRowValue and type(previousRowValue.value) is str:
             if team.endswith('(A)') or team.endswith('(B)'):
-                    if team.endswith('(A)'):
-                        for teamName in teamNamesForFirstTeam:
-                            if teamName.lower() in previousRowValue.value.lower():
-                                return True
+                if team.endswith('(A)'):
+                    for teamName in teamNamesForFirstTeam:
+                        if teamName.lower() in previousRowValue.value.lower():
+                            return True
 
-                    if team.endswith('(B)'):
-                        for teamName in teamNamesForSecondTeam:
-                            if teamName.lower() in previousRowValue.value.lower():
-                                return True
+                if team.endswith('(B)'):
+                    for teamName in teamNamesForSecondTeam:
+                        if teamName.lower() in previousRowValue.value.lower():
+                            return True
 
             else:
                 for teamName in teamNames:
