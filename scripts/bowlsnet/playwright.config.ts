79c22bbf1da1c538bb58bc9a56c8e1bb33394a48
Andrew Marshall <andrew.marshall@ensono.com>
2024-07-17 16:39:36 +0100

    adding reports for bowlsnet script

--- scripts/bowlsnet/playwright.config.ts
+++ scripts/bowlsnet/playwright.config.ts
@@ -11,7 +11,7 @@ const config: PlaywrightTestConfig = {
   forbidOnly: true,
   retries: 2,
   workers: 4,
-  // reporter: 'html',
+  reporter: 'html',
   use: {
     actionTimeout: 0,
     baseURL: 'https://bowlsnet.uk',
