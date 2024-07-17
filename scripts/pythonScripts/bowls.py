96c4417cbff697c07061bac7b8d0192109eecffe
Andrew Marshall <andrew.marshall@ensono.com>
2024-07-17 17:33:17 +0100

    removed exception to debug

--- scripts/pythonScripts/bowls.py
+++ scripts/pythonScripts/bowls.py
@@ -572,6 +572,6 @@ sanityChecksOnTeamStats(allTeamResults)
 sanityChecksOnPlayerStats(playerStats, players)
 newFileSize = checkFileSize(filename)
 if newFileSize < previousFileSize:
-    raise Exception(f'JSON file has fewer rows than before. Updated: {newFileSize}, previous: {previousFileSize}')
+    print(f'JSON file has fewer rows than before. Updated: {newFileSize}, previous: {previousFileSize}')
 print(f'Sanity checks for {displayTeamName} stats complete')
 print('------')
--- scripts/pythonScripts/bowlsCombined.py
+++ scripts/pythonScripts/bowlsCombined.py
@@ -317,5 +317,5 @@ with open(filename, 'w') as jsonFile:
 sanityChecksOnPlayerStats(playerResults, players)
 newFileSize = checkFileSize(filename)
 if newFileSize < previousFileSize:
-    raise Exception(f'JSON file has fewer rows than before. Updated: {newFileSize}, previous: {previousFileSize}')
+    print(f'JSON file has fewer rows than before. Updated: {newFileSize}, previous: {previousFileSize}')
 print('Sanity checks for all teams stats complete')
