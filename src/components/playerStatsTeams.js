0882c07297f84ff26b00471913511fcd1182a60c
Andrew Marshall <andrew.marshall@ensono.com>
2024-07-27 00:57:59 +0100

    hide team accordion if no data available

--- src/components/playerStatsTeams.js
+++ src/components/playerStatsTeams.js
@@ -3,8 +3,6 @@ import { Accordion } from 'react-bootstrap';
 function PlayerStatsTeams(props) {
     const stats = props.stats;
 
-    // TODO sort this out e.g. Cliff in 2013
-
     const {
         mondayGames,
         mondayAvg,
@@ -40,153 +38,173 @@ function PlayerStatsTeams(props) {
         saturdayBAvg,
     } = stats;
 
-    return (
-        <div id="player-stats-teams">
-            <Accordion.Item eventKey="4">
-                <Accordion.Header id="stats-teams">TEAMS</Accordion.Header>
-                <Accordion.Body>
-                    <div>
-                        {mondayGames > 0 && (
-                            <div>
-                                <h3>MONDAY</h3>
-                                <p>Games: {mondayGames}</p>
-                                <p>Wins: {mondayWins}</p>
-                                <p>Losses: {mondayLosses}</p>
-                                <p>Average: {mondayAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {((mondayWins / mondayGames) * 100).toFixed(
-                                        0
-                                    )}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                        {tuesdayVetsGames > 0 && (
-                            <div>
-                                <h3>TUESDAY VETS</h3>
-                                <p>Games: {tuesdayVetsGames}</p>
-                                <p>Wins: {tuesdayVetsWins}</p>
-                                <p>Losses: {tuesdayVetsLosses}</p>
-                                <p>Average: {tuesdayVetsAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {(
-                                        (tuesdayVetsWins / tuesdayVetsGames) *
-                                        100
-                                    ).toFixed(0)}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                        {tuesdayEveningGames > 0 && (
-                            <div>
-                                <h3>TUESDAY EVENING</h3>
-                                <p>Games: {tuesdayEveningGames}</p>
-                                <p>Wins: {tuesdayEveningWins}</p>
-                                <p>Losses: {tuesdayEveningLosses}</p>
-                                <p>Average: {tuesdayEveningAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {(
-                                        (tuesdayEveningWins /
-                                            tuesdayEveningGames) *
-                                        100
-                                    ).toFixed(0)}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                        {wednesdayGames > 0 && (
-                            <div>
-                                <h3>WEDNESDAY HALF HOLIDAY</h3>
-                                <p>Games: {wednesdayGames}</p>
-                                <p>Wins: {wednesdayWins}</p>
-                                <p>Losses: {wednesdayLosses}</p>
-                                <p>Average: {wednesdayAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {(
-                                        (wednesdayWins / wednesdayGames) *
-                                        100
-                                    ).toFixed(0)}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                        {wednesdayPairsGames > 0 && (
-                            <div>
-                                <h3>WEDNESDAY PAIRS</h3>
-                                <p>Games: {wednesdayPairsGames}</p>
-                                <p>Wins: {wednesdayPairsWins}</p>
-                                <p>Losses: {wednesdayPairsLosses}</p>
-                                <p>Average: {wednesdayPairsAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {(
-                                        (wednesdayPairsWins / wednesdayPairsGames) *
-                                        100
-                                    ).toFixed(0)}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                        {thursdayGames > 0 && (
-                            <div>
-                                <h3>THURSDAY VETS</h3>
-                                <p>Games: {thursdayGames}</p>
-                                <p>Wins: {thursdayWins}</p>
-                                <p>Losses: {thursdayLosses}</p>
-                                <p>Average: {thursdayAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {(
-                                        (thursdayWins / thursdayGames) *
-                                        100
-                                    ).toFixed(0)}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                        {saturdayGames > 0 && (
-                            <div>
-                                <h3>SATURDAY A</h3>
-                                <p>Games: {saturdayGames}</p>
-                                <p>Wins: {saturdayWins}</p>
-                                <p>Losses: {saturdayLosses}</p>
-                                <p>Average: {saturdayAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {(
-                                        (saturdayWins / saturdayGames) *
-                                        100
-                                    ).toFixed(0)}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                        {saturdayBGames > 0 && (
-                            <div>
-                                <h3>SATURDAY B</h3>
-                                <p>Games: {saturdayBGames}</p>
-                                <p>Wins: {saturdayBWins}</p>
-                                <p>Losses: {saturdayBLosses}</p>
-                                <p>Average: {saturdayBAvg.toFixed(2)}</p>
-                                <p>
-                                    Win percentage:{' '}
-                                    {(
-                                        (saturdayBWins / saturdayBGames) *
-                                        100
-                                    ).toFixed(0)}
-                                    %
-                                </p>
-                            </div>
-                        )}
-                    </div>
-                </Accordion.Body>
-            </Accordion.Item>
-        </div>
-    );
+    if (
+        mondayGames > 0 ||
+        tuesdayVetsGames > 0 ||
+        tuesdayEveningGames > 0 ||
+        wednesdayGames > 0 ||
+        wednesdayPairsGames > 0 ||
+        thursdayGames > 0 ||
+        saturdayGames > 0 ||
+        saturdayBGames > 0
+    ) {
+        return (
+            <div id="player-stats-teams">
+                <Accordion.Item eventKey="4">
+                    <Accordion.Header id="stats-teams">TEAMS</Accordion.Header>
+                    <Accordion.Body>
+                        <div>
+                            {mondayGames > 0 && (
+                                <div>
+                                    <h3>MONDAY</h3>
+                                    <p>Games: {mondayGames}</p>
+                                    <p>Wins: {mondayWins}</p>
+                                    <p>Losses: {mondayLosses}</p>
+                                    <p>Average: {mondayAvg.toFixed(2)}</p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (mondayWins / mondayGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                            {tuesdayVetsGames > 0 && (
+                                <div>
+                                    <h3>TUESDAY VETS</h3>
+                                    <p>Games: {tuesdayVetsGames}</p>
+                                    <p>Wins: {tuesdayVetsWins}</p>
+                                    <p>Losses: {tuesdayVetsLosses}</p>
+                                    <p>Average: {tuesdayVetsAvg.toFixed(2)}</p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (tuesdayVetsWins /
+                                                tuesdayVetsGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                            {tuesdayEveningGames > 0 && (
+                                <div>
+                                    <h3>TUESDAY EVENING</h3>
+                                    <p>Games: {tuesdayEveningGames}</p>
+                                    <p>Wins: {tuesdayEveningWins}</p>
+                                    <p>Losses: {tuesdayEveningLosses}</p>
+                                    <p>
+                                        Average: {tuesdayEveningAvg.toFixed(2)}
+                                    </p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (tuesdayEveningWins /
+                                                tuesdayEveningGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                            {wednesdayGames > 0 && (
+                                <div>
+                                    <h3>WEDNESDAY HALF HOLIDAY</h3>
+                                    <p>Games: {wednesdayGames}</p>
+                                    <p>Wins: {wednesdayWins}</p>
+                                    <p>Losses: {wednesdayLosses}</p>
+                                    <p>Average: {wednesdayAvg.toFixed(2)}</p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (wednesdayWins / wednesdayGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                            {wednesdayPairsGames > 0 && (
+                                <div>
+                                    <h3>WEDNESDAY PAIRS</h3>
+                                    <p>Games: {wednesdayPairsGames}</p>
+                                    <p>Wins: {wednesdayPairsWins}</p>
+                                    <p>Losses: {wednesdayPairsLosses}</p>
+                                    <p>
+                                        Average: {wednesdayPairsAvg.toFixed(2)}
+                                    </p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (wednesdayPairsWins /
+                                                wednesdayPairsGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                            {thursdayGames > 0 && (
+                                <div>
+                                    <h3>THURSDAY VETS</h3>
+                                    <p>Games: {thursdayGames}</p>
+                                    <p>Wins: {thursdayWins}</p>
+                                    <p>Losses: {thursdayLosses}</p>
+                                    <p>Average: {thursdayAvg.toFixed(2)}</p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (thursdayWins / thursdayGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                            {saturdayGames > 0 && (
+                                <div>
+                                    <h3>SATURDAY A</h3>
+                                    <p>Games: {saturdayGames}</p>
+                                    <p>Wins: {saturdayWins}</p>
+                                    <p>Losses: {saturdayLosses}</p>
+                                    <p>Average: {saturdayAvg.toFixed(2)}</p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (saturdayWins / saturdayGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                            {saturdayBGames > 0 && (
+                                <div>
+                                    <h3>SATURDAY B</h3>
+                                    <p>Games: {saturdayBGames}</p>
+                                    <p>Wins: {saturdayBWins}</p>
+                                    <p>Losses: {saturdayBLosses}</p>
+                                    <p>Average: {saturdayBAvg.toFixed(2)}</p>
+                                    <p>
+                                        Win percentage:{' '}
+                                        {(
+                                            (saturdayBWins / saturdayBGames) *
+                                            100
+                                        ).toFixed(0)}
+                                        %
+                                    </p>
+                                </div>
+                            )}
+                        </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </div>
+        );
+    } else {
+        return null;
+    }
 }
 
 export default PlayerStatsTeams;
