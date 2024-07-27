19169dba0c3b2de0dcf3025e317edb22b943ee0b
Andrew Marshall <andrew.marshall@ensono.com>
2024-07-27 12:14:45 +0100

    removed wednesday airewharfe

--- scripts/pythonScripts/utils.py
+++ scripts/pythonScripts/utils.py
@@ -6,10 +6,9 @@ duplicateTeamMemberNames = teamDetails.duplicateTeamMemberNames
 currentTeamDays = teamDetails.teamDays
 
 # Add Saturday AireDale & Wharfedale and Monday Pairs?
-otherLeagues = ['Tuesday Mirfield', 'Wednesday Spen Valley', 'Monday AireDale & Wharfedale', 'Wednesday AireDale & Wharfedale',
+otherLeagues = ['Tuesday Mirfield', 'Wednesday Spen Valley', 'Monday AireDale & Wharfedale',
                 'Tuesday AireDale & Wharfedale', 'Monday Bradford', 'Wednesday Half Holiday Bradford',
                 'Thursday Vets Bradford', 'Saturday Bradford']
-# TODO remove 'Wednesday AireDale & Wharfedale'
 leaguesWithGamesTo26 = ['wednesday pairs airewharfe']
 teamDays = currentTeamDays + otherLeagues
 extraPlayers = []  # this is to track players who only play for a different team
