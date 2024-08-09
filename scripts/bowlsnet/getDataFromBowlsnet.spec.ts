1ea21051975079629ecc4144848dd083ab3f0e28
Andrew Marshall <andrew.marshall@ensono.com>
2024-08-09 01:41:50 +0100

    changed how the script writes the file

--- scripts/bowlsnet/getDataFromBowlsnet.spec.ts
+++ scripts/bowlsnet/getDataFromBowlsnet.spec.ts
@@ -152,6 +152,6 @@ for (const team of teams) {
     await newPage.bringToFront();
     const value = await newPage.$eval('body > pre', (el) => el.textContent);
     const filePath = `./bowlsnetReports/${year}/${team.day}.txt`;
-    fs.writeFileSync(filePath, value as string);
+    fs.writeFile(filePath, value as string, (err) => {  console.log(err); }); 
   });
 }
