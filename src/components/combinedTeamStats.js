316871547565b0bd2eb5e0fd93e796d500995deb
Andrew Marshall <andrew.marshall@ensono.com>
2024-08-20 00:50:05 +0100

    added more graphs for team stats

--- src/components/combinedTeamStats.js
+++ src/components/combinedTeamStats.js
@@ -1,10 +1,27 @@
 import { combineTeamStats } from '../helpers/statsHelper';
 import StatsTableDisplay from './statsTableDisplay';
 import { Row, Col } from 'react-bootstrap';
-import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie, Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    ArcElement,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    Title
+);
 
 function CombinedTeamStats(props) {
     const stats = props.stats;
@@ -84,13 +101,32 @@ function CombinedTeamStats(props) {
         ],
     };
 
+    const aggregateData = {
+        labels: ['For', 'Against'],
+        datasets: [
+            {
+                label: 'Aggregate', // TODO this label looks weird
+                data: [combinedAgg, combinedOpponentAgg],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: ['rgb(255, 99, 132)', 'rgb(255, 159, 64)'],
+                borderWidth: 2,
+            },
+        ],
+    };
+
     // TODO remove https://www.chartjs.org/docs/latest/charts/doughnut.html
 
     if (totalGames > 0) {
         return (
-            <div id="combined-team-win-losses">
-                <h3>WINS & LOSSES</h3>
+            // TODO these aren't centered
+            <div className="center" id="combined-team-win-losses">
                 <Row sm={1} md={2} lg={3} xl={4} className="g-4 tabs">
+                    <h3>GAMES PLAYED</h3>
+                    <p>{totalGames}</p>
+                    <h3>WINS & LOSSES</h3>
                     <Col>
                         <div style={{ width: '90%' }}>
                             <h4>TOTAL</h4>
@@ -116,8 +152,14 @@ function CombinedTeamStats(props) {
                         </div>
                     </Col>
                 </Row>
+                <h3>AGGREGATES</h3>
+                <Row>
+                    <Col>
+                        <Bar data={aggregateData} />
+                    </Col>
+                </Row>
 
-                {/* TODO also agg for/against and total games */}
+                {/* TODO win percentages? */}
 
                 <StatsTableDisplay
                     totalGames={totalGames}
