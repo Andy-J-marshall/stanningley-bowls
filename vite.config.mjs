72cba516a69f4d2c5ab867c9346040d72fbd8b6a
Andrew Marshall <andrew.marshall@ensono.com>
2024-09-29 22:46:13 +0100

    Refactor vite.config.mjs to remove unused code and simplify plugin configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,20 +1,8 @@
-import { defineConfig, transformWithEsbuild } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        {
-            name: 'treat-ts-files-as-tsx',
-            async transform(code, id) {
-                if (!id.match(/src\/.*\.ts$/)) return null;
-                return transformWithEsbuild(code, id, {
-                    loader: 'tsx',
-                    jsx: 'automatic',
-                });
-            },
-        },
-        react(),
-    ],
+    plugins: [react()],
     optimizeDeps: {
         force: true,
         esbuildOptions: {
