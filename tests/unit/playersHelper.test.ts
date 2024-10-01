7fa5775507e3d1b45991bbf642b4f717bf7d7796
Andrew Marshall <andrew.marshall@ensono.com>
2024-10-02 00:37:53 +0100

    improved unit tests

--- tests/unit/playersHelper.test.ts
+++ tests/unit/playersHelper.test.ts
@@ -4,6 +4,7 @@ import {
     calculatePairsPartnersCount,
     checkWinPercAndAverageAreNumbers,
 } from '../../src/helpers/playersHelper';
+import stats2022 from '../../src/data/bowlsStats2022.json'
 
 describe('#Players Tests', () => {
     describe('#calculatePairsPartnersCount()', () => {
@@ -25,152 +26,8 @@ describe('#Players Tests', () => {
     });
 
     describe('#returnPlayerStats()', () => {
-        let playerStats: any;
-        let stats: any;
-
-        beforeEach(() => {
-            playerStats = {
-                'paul bowes': {
-                    totalAgg: 841,
-                    totalAggAgainst: 457,
-                    homeWins: 18,
-                    homeLosses: 3,
-                    awayWins: 11,
-                    awayLosses: 6,
-                    pairWins: 5,
-                    pairLosses: 4,
-                    pairsPartners: [
-                        'alyssa randell',
-                        'alyssa randell',
-                        'alyssa randell',
-                        'alyssa randell',
-                        'alyssa randell',
-                        'alyssa randell',
-                        'vanessa lancaster',
-                        'vanessa lancaster',
-                        'vanessa lancaster',
-                    ],
-                    dayPlayed: [
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Monday Combined Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Tuesday Vets Leeds',
-                        'Thursday Vets Leeds',
-                        'Thursday Vets Leeds',
-                        'Thursday Vets Leeds',
-                        'Thursday Vets Leeds',
-                        'Thursday Vets Leeds',
-                        'Thursday Vets Leeds',
-                        'Thursday Vets Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                        'Saturday Leeds',
-                    ],
-                    totalPairsAgg: 167,
-                    totalPairsAggAgainst: 140,
-                    winningPairsPartners: [
-                        'alyssa randell',
-                        'alyssa randell',
-                        'alyssa randell',
-                        'alyssa randell',
-                        'vanessa lancaster',
-                    ],
-                    losingPairsPartners: [
-                        'alyssa randell',
-                        'alyssa randell',
-                        'vanessa lancaster',
-                        'vanessa lancaster',
-                    ],
-                    totalHomeAgg: 414,
-                    totalHomeAggAgainst: 205,
-                    totalAwayAgg: 343,
-                    totalAwayAggAgainst: 224,
-                    cupWins: 4,
-                    cupLosses: 0,
-                    results: [
-                        'paul bowes & alyssa randell 17 - 21 malcolm colinson & chris walsh',
-                        'paul bowes & alyssa randell 17 - 21 harvey lockwood & kevin smales',
-                        'paul bowes & alyssa randell 21 - 7 maureen sheridan & pat blackburn',
-                        'paul bowes & alyssa randell 21 - 13 dot taylor & glennys ash',
-                        'paul bowes & alyssa randell 21 - 19 paul shackleton & ann hughes',
-                        'paul bowes & alyssa randell 21 - 9 jade kerry & sharon kerry',
-                        'paul bowes 17 - 21 michael gore',
-                        'paul bowes 21 - 2 linda gaunt',
-                        'paul bowes 21 - 9 cedric ash',
-                        'paul bowes & vanessa lancaster 21 - 8 peter simmons & james wright-green',
-                        'paul bowes & vanessa lancaster 20 - 21 gary dennison & nicola pearson',
-                        'paul bowes & vanessa lancaster 8 - 21 harry dixon & keith skelton',
-                        'paul bowes 11 - 21 tony green',
-                        'paul bowes 21 - 6 martin mcgowan',
-                        'paul bowes 21 - 5 doreen hunter',
-                        'paul bowes 21 - 8 jane boyd',
-                        'paul bowes 21 - 4 steve milton',
-                        'paul bowes 20 - 21 harvey lockwood',
-                        'paul bowes 21 - 11 graham hodder',
-                        'paul bowes 21 - 14 david howard',
-                        'paul bowes 21 - 11 stuart poulter',
-                        'paul bowes 18 - 21 dennis walters',
-                        'paul bowes 21 - 4 brian ragan',
-                        'paul bowes 21 - 13 peter richie',
-                        'paul bowes 20 - 21 lawrie bairstow',
-                        'paul bowes 21 - 4 malcolm cameron',
-                        'paul bowes 21 - 11 derek howell',
-                        'paul bowes 21 - 8 cedric briggs',
-                        'paul bowes 21 - 7 brian newton',
-                        'paul bowes 21 - 0 clifford pitts',
-                        'paul bowes 21 - 6 peter nicholson',
-                        'paul bowes 21 - 13 mick hall',
-                        'paul bowes 21 - 10 ian baker',
-                        'paul bowes 21 - 1 ron lavan',
-                        'paul bowes 21 - 2 michael geary',
-                        'paul bowes 21 - 10 arthur horne',
-                        'paul bowes 21 - 5 bill lovett',
-                        'paul bowes 21 - 3 dawn peters',
-                        'paul bowes 21 - 8 neil schofield',
-                        'paul bowes 21 - 13 harry gillett',
-                        'paul bowes 21 - 14 gordon monk',
-                        'paul bowes 21 - 10 andrew cole',
-                    ],
-                    'monday combined leeds': {
-                        games: 12,
-                        wins: 7,
-                        aggDiff: 54,
-                    },
-                    'tuesday vets leeds': { games: 9, wins: 7, aggDiff: 77 },
-                    'thursday vets leeds': { games: 7, wins: 5, aggDiff: 61 },
-                    'saturday leeds': { games: 14, wins: 14, aggDiff: 192 },
-                },
-            };
-            stats = returnPlayerStats(playerStats, 'paul bowes');
-        });
+        let playerStats: any = stats2022.playerResults;
+        let stats: any = returnPlayerStats(playerStats, 'paul bowes');
 
         it('Games Played', () => {
             const {
@@ -180,11 +37,11 @@ describe('#Players Tests', () => {
                 cupGamesPlayed,
                 awayGamesPlayed,
             } = stats;
-            expect(gamesPlayed).to.equal(42);
-            expect(singlesGames).to.equal(33);
-            expect(homeGamesPlayed).to.equal(21);
+            expect(gamesPlayed).to.equal(53);
+            expect(singlesGames).to.equal(43);
+            expect(homeGamesPlayed).to.equal(25);
             expect(cupGamesPlayed).to.equal(4);
-            expect(awayGamesPlayed).to.equal(17);
+            expect(awayGamesPlayed).to.equal(24);
         });
 
         it('Averages', () => {
@@ -196,28 +53,28 @@ describe('#Players Tests', () => {
                 singlesAvg,
                 pairsAvg,
             } = stats;
-            expect(average.toFixed(2)).to.equal('9.14');
-            expect(homeAverage.toFixed(2)).to.equal('9.95');
+            expect(average.toFixed(2)).to.equal('9.64');
+            expect(homeAverage.toFixed(2)).to.equal('10.48');
             expect(cupAverage.toFixed(2)).to.equal('14.00');
-            expect(awayAverage.toFixed(2)).to.equal('7.00');
-            expect(singlesAvg.toFixed(2)).to.equal('10.82');
-            expect(pairsAvg.toFixed(2)).to.equal('3.00');
+            expect(awayAverage.toFixed(2)).to.equal('8.04');
+            expect(singlesAvg.toFixed(2)).to.equal('11.56');
+            expect(pairsAvg.toFixed(2)).to.equal('1.40');
         });
 
         it('Wins', () => {
             const { totalWins, homeWins, awayWins, cupWins } = stats;
-            expect(totalWins).to.equal(33);
-            expect(homeWins).to.equal(18);
+            expect(totalWins).to.equal(43);
+            expect(homeWins).to.equal(22);
             expect(cupWins).to.equal(4);
-            expect(awayWins).to.equal(11);
+            expect(awayWins).to.equal(17);
         });
 
         it('Losses', () => {
             const { totalLosses, homeLosses, awayLosses, cupLosses } = stats;
-            expect(totalLosses).to.equal(9);
+            expect(totalLosses).to.equal(10);
             expect(homeLosses).to.equal(3);
             expect(cupLosses).to.equal(0);
-            expect(awayLosses).to.equal(6);
+            expect(awayLosses).to.equal(7);
         });
 
         it('Aggregates show correctly', () => {
@@ -235,18 +92,18 @@ describe('#Players Tests', () => {
                 totalPairsAgg,
                 totalPairsAggAgainst,
             } = stats;
-            expect(totalAgg).to.equal(841);
-            expect(totalHomeAgg).to.equal(414);
+            expect(totalAgg).to.equal(1059);
+            expect(totalHomeAgg).to.equal(498);
             expect(cupAgg).to.equal(84);
-            expect(totalAwayAgg).to.equal(343);
-            expect(totalAggAgainst).to.equal(457);
-            expect(totalHomeAggAgainst).to.equal(205);
+            expect(totalAwayAgg).to.equal(477);
+            expect(totalAggAgainst).to.equal(548);
+            expect(totalHomeAggAgainst).to.equal(236);
             expect(cupAggAgainst).to.equal(28);
-            expect(totalAwayAggAgainst).to.equal(224);
-            expect(singlesAgg).to.equal(674);
-            expect(singlesAggAgainst).to.equal(317);
-            expect(totalPairsAgg).to.equal(167);
-            expect(totalPairsAggAgainst).to.equal(140);
+            expect(totalAwayAggAgainst).to.equal(284);
+            expect(singlesAgg).to.equal(884);
+            expect(singlesAggAgainst).to.equal(387);
+            expect(totalPairsAgg).to.equal(175);
+            expect(totalPairsAggAgainst).to.equal(161);
         });
 
         it('Results listed correctly', () => {
@@ -254,7 +111,7 @@ describe('#Players Tests', () => {
             expect(results[0]).to.equal(
                 'paul bowes & alyssa randell 17 - 21 malcolm colinson & chris walsh'
             );
-            expect(results).to.have.lengthOf(42);
+            expect(results).to.have.lengthOf(53);
             expect(biggestWin).to.equal('21 - 0');
         });
 
@@ -312,15 +169,15 @@ describe('#Players Tests', () => {
                 pairWins,
                 pairsGames,
             } = stats;
-            expect(pairLosses).to.equal(4);
+            expect(pairLosses).to.equal(5);
             expect(pairWins).to.equal(5);
-            expect(pairsGames).to.equal(9);
+            expect(pairsGames).to.equal(10);
             expect(pairsPartnersCount['alyssa randell'].timesPaired).to.equal(
                 6
             );
             expect(
                 pairsPartnersCount['vanessa lancaster'].timesPaired
-            ).to.equal(3);
+            ).to.equal(4);
             expect(
                 pairsPartnersCountWins['alyssa randell'].timesPaired
             ).to.equal(4);
@@ -332,7 +189,7 @@ describe('#Players Tests', () => {
             ).to.equal(2);
             expect(
                 pairsPartnersCountLosses['vanessa lancaster'].timesPaired
-            ).to.equal(2);
+            ).to.equal(3);
         });
 
         it('Days Played', () => {
@@ -360,24 +217,24 @@ describe('#Players Tests', () => {
             });
 
             expect(monday.teamWins).to.equal(7);
-            expect(monday.teamLosses).to.equal(5);
-            expect(monday.teamGames).to.equal(12);
-            expect(monday.teamAvg.toFixed(2)).to.equal('4.50');
+            expect(monday.teamLosses).to.equal(6);
+            expect(monday.teamGames).to.equal(13);
+            expect(monday.teamAvg.toFixed(2)).to.equal('3.15');
 
-            expect(tuesVets.teamWins).to.equal(7);
+            expect(tuesVets.teamWins).to.equal(12);
             expect(tuesVets.teamLosses).to.equal(2);
-            expect(tuesVets.teamGames).to.equal(9);
-            expect(tuesVets.teamAvg.toFixed(2)).to.equal('8.56');
+            expect(tuesVets.teamGames).to.equal(14);
+            expect(tuesVets.teamAvg.toFixed(2)).to.equal('10.36');
 
-            expect(thursVets.teamWins).to.equal(5);
+            expect(thursVets.teamWins).to.equal(7);
             expect(thursVets.teamLosses).to.equal(2);
-            expect(thursVets.teamGames).to.equal(7);
-            expect(thursVets.teamAvg.toFixed(2)).to.equal('8.71');
+            expect(thursVets.teamGames).to.equal(9);
+            expect(thursVets.teamAvg.toFixed(2)).to.equal('10.78');
 
-            expect(saturday.teamWins).to.equal(14);
+            expect(saturday.teamWins).to.equal(17);
             expect(saturday.teamLosses).to.equal(0);
-            expect(saturday.teamGames).to.equal(14);
-            expect(saturday.teamAvg.toFixed(2)).to.equal('13.71');
+            expect(saturday.teamGames).to.equal(17);
+            expect(saturday.teamAvg.toFixed(2)).to.equal('13.41');
         });
     });
 
