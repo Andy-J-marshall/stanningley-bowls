21c273bf74e695195a434a59f4d255b3ddbbe8d7
Andrew Marshall <andrew.marshall@ensono.com>
2024-10-22 20:05:23 +0100

    improved images

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import Header from '../components/header';
 import News from '../components/news';
 import HomePageCarousel from '../components/homePageCarousel';
 
+// TODO issue with width
 function Home() {
     return (
         <div className="center" id="home-page">
