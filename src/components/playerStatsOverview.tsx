2de18aff34e12eaca2defc009e35516021281ac6
Andrew Marshall <andrew.marshall@ensono.com>
2024-10-30 15:54:02 +0000

    added cards to stats overview

--- src/components/playerStatsOverview.tsx
+++ src/components/playerStatsOverview.tsx
@@ -1,17 +1,11 @@
-import { Accordion } from 'react-bootstrap';
+import { Accordion, Card, Row } from 'react-bootstrap';
 import { capitalizeText } from '../helpers/utils';
 import { PlayerStatsComponentsProps } from '../types/interfaces';
 
 function PlayerStatsOverview(props: PlayerStatsComponentsProps) {
     const stats = props.stats;
 
-    const {
-        totalLosses,
-        totalWins,
-        gamesPlayed,
-        average,
-        biggestWin,
-    } = stats;
+    const { totalLosses, totalWins, gamesPlayed, average, biggestWin } = stats;
 
     return (
         <div id="player-stats-overview">
@@ -20,27 +14,68 @@ function PlayerStatsOverview(props: PlayerStatsComponentsProps) {
                     OVERVIEW
                 </Accordion.Header>
                 <Accordion.Body>
-                    <div>
-                        <h3>STATS</h3>
-                        <p id="totalGamesPlayed">Games played: {gamesPlayed}</p>
-                        <p id="totalWins">Wins: {totalWins}</p>
-                        <p id="totalLosses">Losses: {totalLosses}</p>
+                    <Row
+                        style={{ margin: '0.2rem' }}
+                        xs={2}
+                        md={3}
+                        className="g-4 align-items-start"
+                    >
+                        <Card bg="light">
+                            <Card.Body>
+                                <Card.Title>GAMES</Card.Title>
+                                <Card.Text id="totalGamesPlayed">
+                                    {gamesPlayed}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card bg="light">
+                            <Card.Body>
+                                <Card.Title>WINS</Card.Title>
+                                <Card.Text id="totalWins">
+                                    {totalWins}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card bg="light">
+                            <Card.Body>
+                                <Card.Title>LOSSES</Card.Title>
+                                <Card.Text id="totalLosses">
+                                    {totalLosses}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                         {average >= -26 && average <= 26 && (
-                            <p id="totalAverage">
-                                Average: {average.toFixed(2)}
-                            </p>
+                            <Card bg="light">
+                                <Card.Body>
+                                    <Card.Title>AVERAGE</Card.Title>
+                                    <Card.Text id="totalAverage">
+                                        {average.toFixed(2)}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         )}
-                        <p>
-                            {' '}
-                            Win percentage:{' '}
-                            {((totalWins / gamesPlayed) * 100).toFixed(0)}%
-                        </p>
+                        <Card bg="light">
+                            <Card.Body>
+                                <Card.Title>WIN %</Card.Title>
+                                <Card.Text id="totalLosses">
+                                    {((totalWins / gamesPlayed) * 100).toFixed(
+                                        0
+                                    )}
+                                    %
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
                         {biggestWin && totalWins > 0 && (
-                            <p id="biggestWin">
-                                Biggest win: {capitalizeText([biggestWin])}
-                            </p>
+                            <Card bg="light">
+                                <Card.Body>
+                                    <Card.Title>AVERAGE</Card.Title>
+                                    <Card.Text id="biggestWin">
+                                        {capitalizeText([biggestWin])}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         )}
-                    </div>
+                    </Row>
                 </Accordion.Body>
             </Accordion.Item>
         </div>
