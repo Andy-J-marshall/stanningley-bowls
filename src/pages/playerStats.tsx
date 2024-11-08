9dfced840a2c16b19681605745ac834038534506
Andrew Marshall <andrew.marshall@ensono.com>
2024-11-08 19:11:35 +0000

    refactored how the all year stats are chosen

--- src/pages/playerStats.tsx
+++ src/pages/playerStats.tsx
@@ -72,26 +72,6 @@ function PlayerStats(props: PlayerStatsProps) {
             window.scrollTo(0, 0);
         }
         setLoaded(true);
-
-        let playersWithGames = [];
-        if (showStatsSinceStart) {
-            playersWithGames = everyYearStatsSummaryArray.filter(
-                (player) => player.games > 0
-            );
-        } else {
-            playersWithGames = statsSummaryArray.filter(
-                (player) => player.games > 0
-            );
-        }
-        setTotalPlayersUsed(playersWithGames.length);
-
-        if (showStatSummary) {
-            setStatsToUse(combinedPlayerResults);
-            setAllYearsStatsToUseArray(combinedStatsForEveryYearArray);
-        } else {
-            setStatsToUse(playerResults);
-            setAllYearsStatsToUseArray(statsForEveryYearArray);
-        }
     });
 
     function scrollToBottom() {
@@ -103,6 +83,19 @@ function PlayerStats(props: PlayerStatsProps) {
     function allYearStatsCallback(showBool: boolean) {
         setShowStatsSinceStart(showBool);
 
+        let playersWithGames = [];
+        if (showBool) {
+            playersWithGames = everyYearStatsSummaryArray.filter(
+                (player) => player.games > 0
+            );
+        } else {
+            playersWithGames = statsSummaryArray.filter(
+                (player) => player.games > 0
+            );
+        }
+
+        setTotalPlayersUsed(playersWithGames.length);
+
         scrollToBottom();
     }
 
@@ -110,8 +103,10 @@ function PlayerStats(props: PlayerStatsProps) {
         setShowStatSummary(showBool);
         if (showBool) {
             setStatsToUse(combinedPlayerResults);
+            setAllYearsStatsToUseArray(combinedStatsForEveryYearArray);
         } else {
             setStatsToUse(playerResults);
+            setAllYearsStatsToUseArray(statsForEveryYearArray);
         }
 
         scrollToBottom();
@@ -255,6 +250,9 @@ function PlayerStats(props: PlayerStatsProps) {
             {!showSinglesOnlyBool &&
                 !searchedPlayerName &&
                 !showPairsOnlyBool &&
+                !showHomeOnlyBool &&
+                !showAwayOnlyBool &&
+                !showCupOnlyBool &&
                 !showStatSummary && (
                     <p id="total-player-count">
                         Total players: {totalPlayersUsed}
