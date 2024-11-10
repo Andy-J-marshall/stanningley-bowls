af17a6fe6f21b90df579bb88e5b0d3d65f337739
Andrew Marshall <andrew.marshall@ensono.com>
2024-11-10 23:34:09 +0000

    added basic link example

--- src/pages/membershipDocs.tsx
+++ src/pages/membershipDocs.tsx
@@ -9,6 +9,11 @@ function MembershipDocs() {
         <div id="member-docs" className="center page-component">
             <h1>membership documents</h1>
             <p>Find the membership documents</p>
+            <div>
+                <a href="/path/to/document1.pdf" download>
+                    Download Document 1
+                </a>
+            </div>
         </div>
     );
 }
