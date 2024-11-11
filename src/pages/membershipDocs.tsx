bd5059b49f319f04b072a2c6318faf48c8da2488
Andrew Marshall <andrew.marshall@ensono.com>
2024-11-11 00:16:50 +0000

    added button to membership page

--- src/pages/membershipDocs.tsx
+++ src/pages/membershipDocs.tsx
@@ -1,61 +1,72 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { config } from '../config';
-import { ListGroup } from 'react-bootstrap';
+import { Button, ListGroup, Offcanvas } from 'react-bootstrap';
 
 function MembershipDocs() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    });
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
-        <div id="member-docs" className="center page-component">
-            <h1>membership documents</h1>
-            <ListGroup>
-                <ListGroup.Item>
-                    <a
-                        target="_blank"
-                        href="https://drive.google.com/file/d/16t4A7AfzcBtd4s9TEEXvKuHFCoPYuakQ/view?usp=drive_link"
-                        download
-                    >
-                        BCGBA Membership Registration Form
-                    </a>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <a
-                        target="_blank"
-                        href="https://drive.google.com/file/d/17-JRHQfRHVJxphj8cDQhsx6pFdMh7u5o/view?usp=drive_link"
-                        download
-                    >
-                        {config.teamNames.shortName} Constitution
-                    </a>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <a
-                        target="_blank"
-                        href="https://drive.google.com/file/d/16kUu4uQ9X9ScvPTap4fRJRT8w28uqIQc/view?usp=drive_link"
-                        download
-                    >
-                        Code of Conduct
-                    </a>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <a
-                        target="_blank"
-                        href="https://drive.google.com/file/d/16vxyxvAqPH311HK6_5zQ0dRW5zfweB7N/view?usp=drive_link"
-                        download
-                    >
-                        Safeguarding Policy
-                    </a>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <a
-                        target="_blank"
-                        href="https://www.bcgba.org.uk/laws-of-the-game/"
-                    >
-                        Crown Green Bowling Rules
-                    </a>
-                </ListGroup.Item>
-            </ListGroup>
+        <div className="center" style={{ padding: '1.5rem' }}>
+            <Button variant="primary" onClick={handleShow}>
+                Membership Documents
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Documents</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <ListGroup>
+                        <ListGroup.Item>
+                            <a
+                                target="_blank"
+                                href="https://drive.google.com/file/d/16t4A7AfzcBtd4s9TEEXvKuHFCoPYuakQ/view?usp=drive_link"
+                                download
+                            >
+                                BCGBA Membership Registration Form
+                            </a>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <a
+                                target="_blank"
+                                href="https://drive.google.com/file/d/17-JRHQfRHVJxphj8cDQhsx6pFdMh7u5o/view?usp=drive_link"
+                                download
+                            >
+                                {config.teamNames.shortName} Constitution
+                            </a>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <a
+                                target="_blank"
+                                href="https://drive.google.com/file/d/16kUu4uQ9X9ScvPTap4fRJRT8w28uqIQc/view?usp=drive_link"
+                                download
+                            >
+                                Code of Conduct
+                            </a>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <a
+                                target="_blank"
+                                href="https://drive.google.com/file/d/16vxyxvAqPH311HK6_5zQ0dRW5zfweB7N/view?usp=drive_link"
+                                download
+                            >
+                                Safeguarding Policy
+                            </a>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <a
+                                target="_blank"
+                                href="https://www.bcgba.org.uk/laws-of-the-game/"
+                            >
+                                Crown Green Bowling Rules
+                            </a>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Offcanvas.Body>
+            </Offcanvas>
         </div>
     );
 }
