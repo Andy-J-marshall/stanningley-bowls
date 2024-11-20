6afa7863bfc252c1fd073437a1c1b63de1047066
Andrew Marshall <andrew.marshall@ensono.com>
2024-11-20 18:41:42 +0000

    renamed image

--- src/components/homePage/homePageCarousel.tsx
+++ src/components/homePage/homePageCarousel.tsx
@@ -1,6 +1,6 @@
 import { Nav, Carousel, Image, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import statsImg from '../../images/carousel/carousel-04.png';
+import statsImg from '../../images/carousel/carousel-01.png';
 import membershipImg from '../../images/carousel/carousel-02.png';
 import eventsImg from '../../images/carousel/carousel-03.png';
 import { config } from '../../config';
