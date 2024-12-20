b02911625007b88bc1ac514d1cf8fdac6bc4f6fa
Andrew Marshall <andrew.marshall@ensono.com>
2024-12-20 16:31:36 +0000

    improved image name for news images

--- src/components/homePage/news.tsx
+++ src/components/homePage/news.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { Button, Card, Collapse, Col, Row } from 'react-bootstrap';
-import news1Img from '../../images/news/news-image1.png';
-import news2Img from '../../images/news/news-image2.png';
-import news3Img from '../../images/news/news-image3.png';
+import news1Img from '../../images/news/fundraising.png';
+import news2Img from '../../images/news/volunteering.png';
+import news3Img from '../../images/news/ladies.png';
 import news from '../../news.json';
 
 function News() {
